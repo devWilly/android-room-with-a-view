022689390562f73ce48d1920ea32fe19c9e0d388
Willy Chang <devwilly666@gmail.com>
2019-04-13 14:55:16 +0800

    implement recyclerView's adapter and viewholder

---
+++ app/src/main/java/com/devwilly/androidroomwordsample/WordListAdapter.kt
@@ -0,0 +1,42 @@
+package com.devwilly.androidroomwordsample
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.devwilly.androidroomwordsample.db.Word
+
+/**
+ * Created by Willy on 2019/4/13.
+ */
+class WordListAdapter(private val context: Context) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+
+    private var mWordList: List<Word> = ArrayList()
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): WordViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.recyclerview_item, p0, false)
+        return WordViewHolder(view)
+    }
+
+    override fun onBindViewHolder(p0: WordViewHolder, p1: Int) {
+        if (mWordList.isNotEmpty()){
+            p0.wordItemTextView.text = mWordList[p1].word
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return mWordList.size
+    }
+
+    fun setWordList(wordList: List<Word>) {
+        mWordList = wordList
+        notifyDataSetChanged()
+    }
+
+    class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val wordItemTextView = itemView.findViewById<TextView>(R.id.textView)
+    }
+}
+
