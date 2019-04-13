86a077cc10d6532102e76c9fc079cc0a4a87c8f9
Willy Chang <devwilly666@gmail.com>
2019-04-13 15:03:23 +0800

    add word database callback to first open wordRoomDataBase

--- app/src/main/java/com/devwilly/androidroomwordsample/db/WordRoomDatabase.kt
+++ app/src/main/java/com/devwilly/androidroomwordsample/db/WordRoomDatabase.kt
@@ -1,9 +1,11 @@
 package com.devwilly.androidroomwordsample.db
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
+import android.os.AsyncTask
 
 /**
  * Created by Willy on 2019/4/13.
@@ -16,17 +18,35 @@ abstract class WordRoomDatabase : RoomDatabase() {
 
     companion object {
         private var INSTANCE: WordRoomDatabase? = null
+        private val sRoomDatabaseCallback: RoomDatabase.Callback = object : RoomDatabase.Callback() {
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+                PopulateDbAsync(INSTANCE!!).execute()
+            }
+        }
 
         fun getDatabase(context: Context) : WordRoomDatabase {
             return INSTANCE ?: synchronized(WordRoomDatabase::class) {
                 val instatnce = Room.databaseBuilder(
                     context.applicationContext,
                     WordRoomDatabase::class.java,
-                    "word_table").build()
+                    "word_table").addCallback(sRoomDatabaseCallback).build()
 
                 INSTANCE = instatnce
                 instatnce
             }
         }
+    }
+
+    private class PopulateDbAsync(db: WordRoomDatabase): AsyncTask<Unit, Unit, Unit>() {
+
+        private val mWordDao: WordDao = db.wordDao()
+
+        override fun doInBackground(vararg params: Unit?) {
+            mWordDao.deleteAll()
+            mWordDao.insert(Word("Hello"))
+            mWordDao.insert(Word("Word!!"))
+        }
+
     }
 }
